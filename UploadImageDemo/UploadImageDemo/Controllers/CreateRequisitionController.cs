fde76d7a5c1dd8563765b83f5d9a53d5a40fac76
tictaclu <lujfeng@hotmail.com>
2016-06-26 17:13:07 +0800

    updated all files

---
+++ UploadImageDemo/UploadImageDemo/Controllers/CreateRequisitionController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UploadImageDemo.Controllers
+{
+    public class CreateRequisitionController : Controller
+    {
+        //
+        // GET: /CreateRequisition/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void UploadImg()
+        {
+           string newSRno = "Abf";
+           // SRimageURL is configured in web.config file. 
+           string strSRimageURL = System.Configuration.ConfigurationManager.AppSettings["SRimageURL"].ToString();
+           string SRPath = Path.Combine(strSRimageURL, newSRno);
+           var file = Request.Files[0];
+           if (file != null && file.ContentLength > 0)
+           {
+               var fileName = Path.GetFileName(file.FileName);
+               var path = Path.Combine(SRPath, fileName);
+               file.SaveAs(path);
+           }
+        }
+	}
+}
---
+++ UploadImageDemo/UploadImageDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UploadImageDemo.Startup))]
+namespace UploadImageDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
